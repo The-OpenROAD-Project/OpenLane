4096d3e4bfb3736942684d8e1d8111f25ff0da3c
agorararmard <aagouhar@efabless.com>
2020-07-21 19:21:17 +0000

    pdn failure detection check added

--- scripts/new_pdn.tcl
+++ scripts/new_pdn.tcl
@@ -1,6 +1,10 @@
 read_lef $::env(MERGED_LEF_UNPADDED)
 read_def $::env(CURRENT_DEF)
 
-pdngen $::env(PDN_CFG) -verbose
+
+if {[catch {pdngen $::env(PDN_CFG) -verbose} errmsg]} {
+    puts stderr $errmsg
+    exit 1
+}
 
 write_def $::env(pdn_tmp_file_tag).def
