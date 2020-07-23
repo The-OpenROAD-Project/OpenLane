4d72230e1421273a6070618c8feddcc2dab0dcd1
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-07-23 14:29:11 +0200

    Correct DRC report path in magic info message
    - nuke.tcl using the openlane_utils package

--- nuke.tcl
+++ nuke.tcl
@@ -1,6 +1,14 @@
 #!/usr/bin/tclsh8.5
-source ./scripts/utils/utils.tcl
+set ::env(OPENLANE_ROOT) [file dirname [file normalize [info script]]]
+lappend ::auto_path "$::env(OPENLANE_ROOT)/scripts/"
 
-puts_warn "\[WARN\]: DELETING ALL DESIGN RUNS!"
-exec rm -rf {*}[glob designs/*/runs]
-puts_success "\[SUCCESS\]: ALL DESIGN RUNS ARE DELETED!"
+package require openlane_utils
+
+puts_warn "DELETING ALL DESIGN RUNS!"
+
+if { ! [catch {glob designs/*/runs} runs] } {
+	file delete -force  {*}$runs
+	puts_success "ALL DESIGN RUNS ARE DELETED!"
+} else {
+	puts_info "No runs to delete"
+}
