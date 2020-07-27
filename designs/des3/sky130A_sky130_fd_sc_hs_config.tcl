98ff2240557131490507a7f45df128275a70fb23
agorararmard <aagouhar@efabless.com>
2020-07-27 21:43:14 +0200

    missing design configurations added

---
+++ designs/des3/sky130A_sky130_fd_sc_hs_config.tcl
@@ -0,0 +1,28 @@
+set ::env(GLB_RT_ADJUSTMENT) 0.1
+set ::env(SYNTH_STRAT) 2
+set ::env(FP_CORE_UTIL) 35
+set ::env(PL_TARGET_DENSITY) 0.2
+#Routing
+set ::env(ROUTING_STRATEGY) 0
+
+# Regression
+set ::env(ROUTING_STRATEGY) 3
+
+# Extra
+
+# Regression
+set ::env(FP_CORE_UTIL) 30
+set ::env(PL_TARGET_DENSITY) 0.10
+set ::env(GLB_RT_ADJUSTMENT) 0.1
+
+
+# Regression
+set ::env(FP_CORE_UTIL) 20
+set ::env(PL_TARGET_DENSITY) 0.10
+
+# Extra# Regression
+set ::env(SYNTH_MAX_FANOUT) 7
+
+# Extra
+set ::env(PDK_VARIANT) sky130_fd_sc_hd
+
--- designs/usb/sky130A_sky130_fd_sc_ms_config.tcl
+++ designs/usb/sky130A_sky130_fd_sc_ms_config.tcl
@@ -1,18 +1,18 @@
-
 set ::env(PL_TARGET_DENSITY) 0.5
 set ::env(FP_CORE_UTIL)      50
 set ::env(SYNTH_STRATEGY)    2
 
+
 set ::env(CLOCK_PERIOD) "10"
-set ::env(FP_CORE_UTIL) 55
-set ::env(PL_TARGET_DENSITY) 0.6
+set ::env(FP_CORE_UTIL) 60
+set ::env(PL_TARGET_DENSITY) 0.65
 set ::env(SYNTH_MAX_FANOUT) 6
 
 #Routing
 set ::env(ROUTING_STRATEGY) 0
-set ::env(FP_CORE_UTIL) 40
-set ::env(PL_TARGET_DENSITY) 0.35
+
+# Regression
+set ::env(FP_CORE_UTIL) 65
+set ::env(PL_TARGET_DENSITY) 0.75
 
 # Extra
-set ::env(PDK_VARIANT) sky130_fd_sc_hd
-
