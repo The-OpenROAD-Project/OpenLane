4412aa54b1821894eb1a8b4ccf6d9f398a83ca48
agorararmard <aagouhar@efabless.com>
2020-08-05 17:01:29 +0200

    update base configs for huge designs

--- designs/biriscv_tcm/config.tcl
+++ designs/biriscv_tcm/config.tcl
@@ -10,8 +10,9 @@ set ::env(CLOCK_PERIOD) "15.000"
 set ::env(CLOCK_PORT) "clk_i"
 
 
-set ::env(CLOCK_NET) "clk_i"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(SYNTH_NO_FLAT) 1
+
 set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
--- designs/biriscv_tcm/sky130A_sky130_fd_sc_hs_config.tcl
+++ designs/biriscv_tcm/sky130A_sky130_fd_sc_hs_config.tcl
@@ -1,2 +1,7 @@
 set ::env(RUN_MAGIC) 1
 set ::env(RUN_ROUTING_DETAILED) 1
+
+
+set ::env(ROUTING_STRATEGY) 14
+set ::env(PL_TARGET_DENSITY) 0.2
+set ::env(FP_CORE_UTIL) 25
