0800ca10191bc5be2d87caa7a83af987cfaea5aa
agorararmard <aagouhar@efabless.com>
2020-08-10 16:28:50 +0200

    print all reports in travis log

--- travisCI/travisTest.sh
+++ travisCI/travisTest.sh
@@ -18,10 +18,11 @@ export PDK_ROOT=$(pwd)/pdks
 export RUN_ROOT=$(pwd)
 echo $PDK_ROOT
 echo $RUN_ROOT
-docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "python3 run_designs.py -d $(cat $RUN_ROOT/travisCI/$test_set) -t TEST_$test_set -th 20 -b regression_results/benchmark_results/SW_HD.csv -p 30"
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc3  bash -c "python3 run_designs.py -d $(cat $RUN_ROOT/travisCI/$test_set) -t TEST_$test_set -th 20 -b regression_results/benchmark_results/SW_HD.csv -p 30"
 
 FILE=$RUN_ROOT/regression_results/TEST_$test_set*/TEST_$test_set*_design_test_report.csv
-echo "design, status"
+cat $RUN_ROOT/regression_results/TEST_$test_set*/TEST_$test_set*.csv
+echo "design,status"
 cat $FILE
 val=$(grep "FAILED" $FILE | wc -l)
 if ! [[ $val ]]; then val=0; fi
