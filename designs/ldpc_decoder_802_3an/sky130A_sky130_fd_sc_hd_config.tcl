b2f1b8f29c80f00987196f3aaeca88666959b029
agorararmard <aagouhar@efabless.com>
2020-08-28 23:43:51 +0200

    designs & CI updates
    - updated the configs of 3 of the previously failing designs to a more stable point. (2 of them are still failing but in later stages)
    - Reordered the CI testcases to increase efficiency
    - Added an extra testcase (a design that its runtime improved) and testing it for timeout

--- designs/ldpc_decoder_802_3an/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/ldpc_decoder_802_3an/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,2 +1,2 @@
-set ::env(FP_CORE_UTIL) 40
-set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)-10) / 100.0 ] 
+set ::env(FP_CORE_UTIL) 15
+set ::env(PL_TARGET_DENSITY) 0.15
--- designs/sound/sky130A_sky130_fd_sc_hdll_config.tcl
+++ designs/sound/sky130A_sky130_fd_sc_hdll_config.tcl
@@ -1,9 +1,8 @@
-
 set ::env(SYNTH_STRATEGY) 2
 set ::env(SYNTH_MAX_FANOUT) 9
-set ::env(CLOCK_PERIOD) "10.000"
 
 
 
-set ::env(FP_CORE_UTIL) 25
-set ::env(PL_TARGET_DENSITY) 0.2
+
+set ::env(FP_CORE_UTIL) 15
+set ::env(PL_TARGET_DENSITY) 0.15
