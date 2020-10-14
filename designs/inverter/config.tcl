19a3e52caf75e162d87f8f420327c6e2fbea9a59
Matt Venn <matt@mattvenn.net>
2020-10-14 10:15:43 +0200

    some comments

--- designs/inverter/config.tcl
+++ designs/inverter/config.tcl
@@ -6,11 +6,12 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 
 # https://github.com/efabless/openlane/blob/master/configuration/README.md
 
-# Fill this
+# turn off clock
 set ::env(CLOCK_PERIOD) 0
 set ::env(CLOCK_TREE_SYNTH) 0
 set ::env(CLOCK_PORT) ""
 
+# without this configuration the process will fail because the design is so small
 set ::env(FP_CORE_UTIL) 5;
 set ::env(PL_TARGET_DENSITY) 0.5
 
