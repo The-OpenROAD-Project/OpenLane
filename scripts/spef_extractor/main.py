7e4389cb6cfb67b707358ec5427a928f24f86411
Tristan Gingold <tgingold@free.fr>
2020-10-15 07:24:33 +0200

    spef_extractor: simplify code

--- scripts/spef_extractor/main.py
+++ scripts/spef_extractor/main.py
@@ -292,8 +292,7 @@ class SpefExtractor:
         pin = [[((point[0], point[1]),
                  (point[0], point[1]),
                  layer)],
-               netName,
-               str(self.pinCounter)]
+               '{}:{}'.format(netName, self.pinCounter)]
         self.pinCounter += 1
         pinsTable.append(pin)
         return pin
@@ -362,15 +361,16 @@ class SpefExtractor:
                 continue
             if con[0] == "PIN":
                 # It is an external PAD
-                x = self.def_parser.pins.get_pin(con[1])
+                pinName = con[1]
+                x = self.def_parser.pins.get_pin(pinName)
                 if x.direction == "INPUT":
                     pin_dir = "I"
                 else:
                     pin_dir = "O"
-                current_pin = ["*P", con[1], pin_dir]
+                current_pin = ["*P", pinName, pin_dir]
 
                 # these are used for the pinsTable
-                pin = self.def_parser.pins.pin_dict[con[1]]
+                pin = self.def_parser.pins.pin_dict[pinName]
                 pinLocation = pin.placed
                 metalLayer = pin.layer.name
                 locationsOfCurrentPin = [((pinLocation[0], pinLocation[1]),
@@ -398,14 +398,15 @@ class SpefExtractor:
                     pin_dir = "I"
                 else:
                     pin_dir = "O"
-                current_pin = ["*I", con[0] + ":" + con[1], pin_dir]
+                pinName = con[0] + ":" + con[1]
+                current_pin = ["*I", pinName, pin_dir]
 
                 # this is used for the pins table
                 metalLayer = pinInfo.info['PORT'].info['LAYER'][0].name
                 locationsOfCurrentPin = self.getPinLocation(con[0], con[1], metalLayer)
 
             # we append list of pin locations - cellName - pinName
-            pinsTable.append((locationsOfCurrentPin, con[0], con[1]))
+            pinsTable.append((locationsOfCurrentPin, pinName))
             conList.append(current_pin)
 
 
@@ -475,9 +476,9 @@ class SpefExtractor:
                     capacitance = self.get_wire_capacitance_modified(spoint, epoint, segment.layer)
 
                 # the name of the first node of the segment
-                currentSNodeName = snode[1] + ':' + snode[2]
+                currentSNodeName = snode[1]
                 # the name of the second node of the segment
-                currentENodeName = enode[1] + ':' + enode[2]
+                currentENodeName = enode[1]
 
                 if wireModel == 'PI':
                     # PI model: add half the capacitances at each of
@@ -498,15 +499,7 @@ class SpefExtractor:
                     else:
                         currentNodeList[currentSNodeName] = capacitance
 
-                if snode[1] != 'PIN':
-                    sname = snode[1] + ':' + snode[2]
-                else:
-                    sname = snode[2]
-                if enode[1] != 'PIN':
-                    ename = enode[1] + ':' + enode[2]
-                else:
-                    ename = node[2]
-                segmentsList.append([sname, ename, resistance])
+                segmentsList.append([currentSNodeName, currentENodeName, resistance])
 
         return {'conn': conList, 'cap': currentNodeList, 'segments': segmentsList}
 
