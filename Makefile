279ca4dab26d0830aee6e9141476784c3f4e7279
agorararmard <aagouhar@efabless.com>
2020-10-15 19:06:37 +0200

    minor Makefile & travisBuild updates

--- Makefile
+++ Makefile
@@ -79,6 +79,7 @@ build-pdk: $(PDK_ROOT)/open_pdks $(PDK_ROOT)/skywater-pdk
 	cd $(PDK_ROOT)/open_pdks && \
 		./configure --with-sky130-source=$(PDK_ROOT)/skywater-pdk/libraries --with-sky130-local-path=$(PDK_ROOT) && \
 		cd sky130 && \
+		$(MAKE) veryclean && \
 		$(MAKE) && \
 		$(MAKE) install-local
 
--- travisCI/travisBuild.sh
+++ travisCI/travisBuild.sh
@@ -18,8 +18,9 @@ echo $PDK_ROOT
 echo $RUN_ROOT
 make openlane
 make skywater-pdk
-make clone-open_pdks
-docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc4  bash -c "make install-open_pdks"
+make skywater-library
+make open_pdks
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc4  bash -c "make build-pdk"
 echo "done installing"
 cd $RUN_ROOT
 exit 0
