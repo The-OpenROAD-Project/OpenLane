94740b813c0cc3cb121d419441131ba91a58f2d0
agorararmard <aagouhar@efabless.com>
2020-10-16 12:32:18 +0000

    151 mem reduction, designs config updates

--- designs/salsa20/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/salsa20/sky130A_sky130_fd_sc_hd_config.tcl
@@ -5,7 +5,6 @@ set ::env(FP_CORE_UTIL) 40
 #Routing
 
 
-set ::env(CLOCK_PERIOD) "20"
 set ::env(FP_CORE_UTIL) 45
 
 set ::env(SYNTH_MAX_FANOUT) 6
@@ -13,4 +12,4 @@ set ::env(SYNTH_MAX_FANOUT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)-10) / 100.0 ] 
 
 # Suggested Clock Period:
- set ::env(CLOCK_PERIOD) "24.73"
+ set ::env(CLOCK_PERIOD) "33.73"
