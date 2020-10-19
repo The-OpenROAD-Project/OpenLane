3ad4e7f4893aeb226b1ce6e38b993948ba7463c3
agorararmard <aagouhar@efabless.com>
2020-10-19 16:18:12 +0200

    workaround FR caveats with designs bigger than 10K cells

--- scripts/openroad/or_route.tcl
+++ scripts/openroad/or_route.tcl
@@ -51,6 +51,22 @@ if { $::env(DIODE_INSERTION_STRATEGY) == 3 } {
 }
 
 
-
 write_guides $::env(fastroute_tmp_file_tag).guide
 write_def $::env(SAVE_DEF)
+
+if {  $::env(DIODE_INSERTION_STRATEGY) != 3 && $::env(GLB_RT_ESTIMATE_PARASITICS) == 1 } {
+    read_liberty -max $::env(LIB_SLOWEST)
+    read_liberty -min $::env(LIB_FASTEST)
+    read_sdc -echo $::env(BASE_SDC_FILE)
+
+    set_wire_rc -layer $::env(WIRE_RC_LAYER)
+    estimate_parasitics -global_routing
+
+    report_checks -unique -slack_max -0.0 -group_count 100 > $::env(fastroute_report_file_tag).timing.rpt
+    report_checks -path_delay min_max > $::env(fastroute_report_file_tag).min_max.rpt
+    report_checks -group_count 100  -slack_max -0.01 > $::env(fastroute_report_file_tag).rpt
+
+    report_wns > $::env(fastroute_report_file_tag)_wns.rpt
+    report_tns > $::env(fastroute_report_file_tag)_tns.rpt
+    
+}
