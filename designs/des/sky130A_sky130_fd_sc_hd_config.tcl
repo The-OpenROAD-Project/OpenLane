3b4d65dbe4047158d23014c942cacbbcf3628781
agorararmard <aagouhar@efabless.com>
2020-11-09 20:59:46 +0200

    small design config update

--- designs/des/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/des/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,7 +1,7 @@
 set ::env(GLB_RT_ADJUSTMENT) 0.1
 set ::env(SYNTH_STRATEGY) 2
 set ::env(CLOCK_PERIOD) "11"
-set ::env(FP_CORE_UTIL) 55
+set ::env(FP_CORE_UTIL) 50
 
 set ::env(SYNTH_MAX_FANOUT) 6
 
