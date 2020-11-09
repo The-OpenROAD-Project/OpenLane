2fc643d47e680a172bac3e6973cda66857516d19
agorararmard <aagouhar@efabless.com>
2020-11-09 20:57:13 +0000

    update des configs

--- designs/des/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/des/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,7 +1,7 @@
 set ::env(GLB_RT_ADJUSTMENT) 0.1
 set ::env(SYNTH_STRATEGY) 2
 set ::env(CLOCK_PERIOD) "11"
-set ::env(FP_CORE_UTIL) 50
+set ::env(FP_CORE_UTIL) 45
 
 set ::env(SYNTH_MAX_FANOUT) 6
 
@@ -15,4 +15,4 @@ set ::env(SYNTH_MAX_FANOUT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)-10) / 100.0 ] 
 
 # Suggested Clock Period:
- set ::env(CLOCK_PERIOD) "11.0"
+ set ::env(CLOCK_PERIOD) "20.0"
