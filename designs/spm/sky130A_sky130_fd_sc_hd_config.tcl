80befd3d0e5e78cd36e37c61e27988d50eb83a4b
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-11-10 21:48:06 +0200

    Update spm config
    - Similar adjustments need to be done to other designs

--- designs/spm/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/spm/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,18 +1,13 @@
-#Routing
-
-
 set ::env(CLOCK_PERIOD) "10"
-set ::env(FP_CORE_UTIL) 60
-
 set ::env(SYNTH_MAX_FANOUT) 5
 
 # Regression
-set ::env(FP_CORE_UTIL) 65
+set ::env(FP_CORE_UTIL) 50
 
 
 # Extra
 
-set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)-10) / 100.0 ] 
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ] 
 
 # Suggested Clock Period:
  set ::env(CLOCK_PERIOD) "10.0"
