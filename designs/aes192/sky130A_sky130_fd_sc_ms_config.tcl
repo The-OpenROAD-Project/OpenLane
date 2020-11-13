eb88e2bab657ba4f6bc0866aa2a1c94a5ec37173
agorararmard <aagouhar@efabless.com>
2020-11-13 21:26:38 +0200

    unlearn ms configs

--- designs/aes192/sky130A_sky130_fd_sc_ms_config.tcl
+++ designs/aes192/sky130A_sky130_fd_sc_ms_config.tcl
@@ -1,7 +1,8 @@
 # SCL Configs
 set ::env(GLB_RT_ADJUSTMENT) 0.1
 set ::env(SYNTH_STRATEGY) 2
-set ::env(FP_CORE_UTIL) 30
-set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(CLOCK_PERIOD) "10.000"
+set ::env(FP_CORE_UTIL) 25
+set ::env(SYNTH_MAX_FANOUT) 9
+set ::env(CLOCK_PERIOD) "12.37"
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CELL_PAD) 4
--- designs/digital_pll_sky130_fd_sc_hd/sky130A_sky130_fd_sc_ms_config.tcl
+++ designs/digital_pll_sky130_fd_sc_hd/sky130A_sky130_fd_sc_ms_config.tcl
@@ -1,7 +1,6 @@
 # SCL Configs
-set ::env(SYNTH_STRATEGY) 2
-set ::env(CLOCK_PERIOD) "17.0"
-set ::env(FP_CORE_UTIL) 35
 set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(FP_CORE_UTIL) 35
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CLOCK_PERIOD) "15.8"
 set ::env(CELL_PAD) 4
