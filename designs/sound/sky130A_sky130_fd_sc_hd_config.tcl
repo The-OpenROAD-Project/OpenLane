88a723c8e1374b3e757e9470a91323427d79f3fb
agorararmard <aagouhar@efabless.com>
2020-11-18 21:55:16 +0000

    config update

--- designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,5 +1,6 @@
 # SCL Configs
 set ::env(SYNTH_STRATEGY) 2
 set ::env(SYNTH_MAX_FANOUT) 9
-set ::env(FP_CORE_UTIL) 20
-set ::env(PL_TARGET_DENSITY) 0.15
+set ::env(FP_CORE_UTIL) 15
+set ::env(PL_TARGET_DENSITY) 0.20
+set ::env(CELL_PAD) 4
