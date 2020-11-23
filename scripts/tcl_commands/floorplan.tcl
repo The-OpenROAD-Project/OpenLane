40e97e34a34d11727d037401956c3c1e4792cc86
ax3ghazy <ax3ghazy@aucegypt.edu>
2020-11-23 20:10:50 +0200

    Remove extra placement step after manual MP

--- scripts/tcl_commands/floorplan.tcl
+++ scripts/tcl_commands/floorplan.tcl
@@ -219,7 +219,6 @@ proc run_floorplan {args} {
 			if { [info exist ::env(MACRO_PLACEMENT_CFG)] } {
 				file copy -force $::env(MACRO_PLACEMENT_CFG) $::env(TMP_DIR)/macro_placement.cfg
 				manual_macro_placement f
-				global_placement_or
 			} else {
 				global_placement_or
 				basic_macro_placement
