7903419e9bfa02584be154c23eddfda5bec5043d
agorararmard <aagouhar@efabless.com>
2020-12-04 17:58:04 +0000

    update design configs

--- designs/151/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/151/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,5 +1,5 @@
 # SCL Configs
 set ::env(CLOCK_PERIOD) 50
-set ::env(FP_CORE_UTIL) 20
-set ::env(PL_TARGET_DENSITY) 0.24
+set ::env(FP_CORE_UTIL) 25
+set ::env(PL_TARGET_DENSITY) 0.30
 set ::env(CELL_PAD) 4
--- designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,11 +1,9 @@
 # SCL Configs
 set ::env(SYNTH_STRATEGY) 2
 set ::env(SYNTH_MAX_FANOUT) 9
-set ::env(FP_CORE_UTIL) 15
-set ::env(PL_TARGET_DENSITY) 0.20
 set ::env(CELL_PAD) 4
 # Regression
-set ::env(FP_CORE_UTIL) 20
+set ::env(FP_CORE_UTIL) 25
 
 # Extra
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
