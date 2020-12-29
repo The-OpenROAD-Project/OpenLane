31cc680097692601e36f92b25d8faf6507a3348f
Anton Blanchard <anton@linux.ibm.com>
2020-12-29 18:37:26 +1100

    Fix manufacturability_report typo

--- scripts/utils/utils.tcl
+++ scripts/utils/utils.tcl
@@ -222,7 +222,7 @@ proc generate_final_summary_report {args} {
     parse_key_args "generate_final_summary_report" args arg_values $options flags_map $flags
     
     set_if_unset arg_values(-output) $::env(REPORTS_DIR)/final_summary_report.csv
-    set_if_unset arg_values(-man_report) $::env(REPORTS_DIR)/manfucturability_report.rpt
+    set_if_unset arg_values(-man_report) $::env(REPORTS_DIR)/manufacturability_report.rpt
 
     if { $::env(GENERATE_FINAL_SUMMARY_REPORT) == 1 } {
         try_catch python3 $::env(OPENLANE_ROOT)/report_generation_wrapper.py -d $::env(DESIGN_DIR) -dn $::env(DESIGN_NAME) -t $::env(RUN_TAG) -o $arg_values(-output) -m $arg_values(-man_report) -r $::env(RUN_DIR)
