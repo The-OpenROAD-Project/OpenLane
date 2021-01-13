84b45c196dd9d9913606c2bedd7442dce7a1574f
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2021-01-13 20:53:23 +0200

    Add KLayout def2gds from OpenROAD
    - TODO: XORing GDSes from KLayout vs. magic automatically
    - Also added licenses on other scripts

--- scripts/klayout/def2gds.sh
+++ scripts/klayout/def2gds.sh
@@ -13,10 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-: ${1?"Usage: $0 file.gds src_layer/src_purpose targ_layer/targ_purpose [output.gds]"}
-: ${2?"Usage: $0 file.gds src_layer/src_purpose targ_layer/targ_purpose [output.gds]"}
-: ${3?"Usage: $0 file.gds src_layer/src_purpose targ_layer/targ_purpose [output.gds]"}
-: ${PDK_ROOT?"You need to export PDK_ROOT"}
-TECH=${TECH:-sky130A}
+export KLAYOUT_SKY130_TECH=/home/xrex/.klayout/tech/SKY130/SKY130.lyt
 
-xvfb-run klayout -z -rd input_layout=$1 -rd tech_file=$PDK_ROOT/$TECH/libs.tech/klayout/$TECH.lyt -rd source_layer=$2 -rd target_layer=$3 -rd output_layout=$4 -rm $(dirname $0)/mv_shapes.py
+: ${1?"Usage: $0 input.def design_name prereq1.gds prereq2.gds ...."}
+: ${2?"Usage: $0 input.def design_name prereq1.gds prereq2.gds ...."}
+: ${3?"Usage: $0 input.def design_name prereq1.gds prereq2.gds ...."}
+
+xvfb-run klayout -z -rd design_name=$2 \
+        -rd in_def=$1 \
+        -rd in_gds="${@:3}" \
+        -rd config_file="" \
+        -rd seal_gds="" \
+        -rd out_gds=$(dirname $1)/$2.gds \
+        -rd tech_file=$KLAYOUT_SKY130_TECH \
+        -rm $(dirname $0)/def2gds.py
