1fcf7ff12bd995352fc1cff8b2ead3a457cf98ae
agorararmard <aagouhar@efabless.com>
2021-01-20 20:54:46 +0200

    [CI] unify all CI branches to develop
    - develop-latest_tools will be replaced with develop-latest_tools_x
    - Removed all the unneeded submoduling and hectic prep scripts with a simple line removal.
    - unified .travis.yml to handle all branches.

---
+++ travisCI/travisPreBuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2020 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "Running the tool prebuild step installation process..."
+export RUN_ROOT=$(pwd)
+echo "Modifying Docker for $TOOL"
+bash $RUN_ROOT/travisCI/utils/remove_line_from_file.sh $RUN_ROOT/docker_build/docker/$TOOL/Dockerfile  "RUN git checkout"
+cd $RUN_ROOT/docker_build
+echo "Re-building $TOOL"
+make build-$TOOL
+echo "done pre-build"
+cd $RUN_ROOT
+exit 0
