1e837aca2e0d224755d3151ab75dc05ccee1af13
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2021-02-08 15:31:08 +0200

    Add a synthesis flag to control resource sharing

--- configuration/synthesis.tcl
+++ configuration/synthesis.tcl
@@ -16,6 +16,7 @@
 set ::env(SYNTH_BIN) yosys
 set ::env(SYNTH_SCRIPT) $::env(SCRIPTS_DIR)/synth.tcl
 set ::env(SYNTH_NO_FLAT) 0
+set ::env(SYNTH_SHARE_RESOURCES) 1
 set ::env(SYNTH_BUFFERING) 1
 set ::env(SYNTH_SIZING) 0
 set ::env(SYNTH_MAX_FANOUT) 5
