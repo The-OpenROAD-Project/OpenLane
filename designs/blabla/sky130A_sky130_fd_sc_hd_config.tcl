bc34f3abda028d84945f01b67cd9f45c001d1a3f
agorararmard <aagouhar@efabless.com>
2021-02-09 18:32:21 +0200

    config updates

--- designs/blabla/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/blabla/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,7 +1,7 @@
 # SCL Configs
 set ::env(GLB_RT_ADJUSTMENT) 0.1
 
-set ::env(FP_CORE_UTIL) 19
+set ::env(FP_CORE_UTIL) 13
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 set ::env(CLOCK_PERIOD) "65.0"
 set ::env(CELL_PAD) 6
--- designs/point_add/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/point_add/sky130A_sky130_fd_sc_hd_config.tcl
@@ -1,7 +1,6 @@
 # SCL Configs
 set ::env(GLB_RT_ADJUSTMENT) 0.15
-set ::env(SYNTH_STRAT) 2
-set ::env(FP_CORE_UTIL) 30
+set ::env(FP_CORE_UTIL) 25
 
 set ::env(SYNTH_MAX_FANOUT) 5
 set ::env(CLOCK_PERIOD) "10.0"
