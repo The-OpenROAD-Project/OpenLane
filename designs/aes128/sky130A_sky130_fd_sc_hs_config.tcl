dc43cc53bd2b7c04e76206f1fd755d862c309930
agorararmard <aagouhar@efabless.com>
2021-02-22 20:51:52 +0200

    reset HS configs to HD

--- designs/aes128/sky130A_sky130_fd_sc_hs_config.tcl
+++ designs/aes128/sky130A_sky130_fd_sc_hs_config.tcl
@@ -1,11 +1,7 @@
 # SCL Configs
 set ::env(SYNTH_MAX_FANOUT) 7
 
-set ::env(FP_CORE_UTIL) 30
-set ::env(FP_PDN_VPITCH) 153.6
-set ::env(FP_PDN_HPITCH) 153.18
-set ::env(FP_ASPECT_RATIO) 1
+set ::env(FP_CORE_UTIL) 25
 set ::env(GLB_RT_ADJUSTMENT) 0.1
-set ::env(FILL_INSERTION) 1
 set ::env(CLOCK_PERIOD) "11.43"
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
--- designs/cic_decimator/sky130A_sky130_fd_sc_hs_config.tcl
+++ designs/cic_decimator/sky130A_sky130_fd_sc_hs_config.tcl
@@ -8,4 +8,4 @@ set ::env(CLOCK_PERIOD) "10.0"
 set ::env(FP_CORE_UTIL) 45
 set ::env(SYNTH_MAX_FANOUT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
-set ::env(CELL_PAD) 4
+
