1344c06a9da8e18103d82295fff80f52af62f999
Amr A. Gouhar <aagouhar@efabless.com>
2021-02-22 23:16:00 +0200

    Config and benchmark updates (#252)
    * designs config updates
    
    * SW_HD benchmark updates

--- designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/sound/sky130A_sky130_fd_sc_hd_config.tcl
@@ -3,7 +3,7 @@
 set ::env(SYNTH_MAX_FANOUT) 9
 set ::env(CELL_PAD) 4
 # Regression
-set ::env(FP_CORE_UTIL) 25
+set ::env(FP_CORE_UTIL) 20
 
 # Extra
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
