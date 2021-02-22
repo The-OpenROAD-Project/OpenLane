4ae1d7447d829628008bdafbf03732dcf5996ed0
agorararmard <aagouhar@efabless.com>
2021-02-22 20:51:42 +0200

    reset HDLL configs to HD

--- designs/salsa20/sky130A_sky130_fd_sc_hdll_config.tcl
+++ designs/salsa20/sky130A_sky130_fd_sc_hdll_config.tcl
@@ -1,5 +1,5 @@
 # SCL Configs
-set ::env(FP_CORE_UTIL) 45
+set ::env(FP_CORE_UTIL) 20
 set ::env(SYNTH_MAX_FANOUT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
-set ::env(CLOCK_PERIOD) "33.73"
+set ::env(CLOCK_PERIOD) "36.73"
