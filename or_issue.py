7f9aab14dba7571d638801bc71d396b8089d3ca0
Donn <me@donn.website>
2021-06-08 11:22:33 +0200

    Reflect changes based on @maliberty's review

--- or_issue.py
+++ or_issue.py
@@ -29,23 +29,20 @@ parser = argparse.ArgumentParser(description="""
 This script creates a reproducible, self-contained package of files to demonstrate
 OpenROAD behavior in a vaccum, suitable for filing issues.
 
-The result is a tarred, xzipped and gzipped tarball for minimal size while
-maintaining GitHub uploadability.
-
 Requires UNIX-like operating system. Final package printed to /dev/stdout. Don't forget to chomp/rstrip.
 
 Usage example: IMAGE_NAME=efabless/openlane:v0.15 python3 or_issue.py -s ./scripts/openroad/or_pdn.tcl ./designs/inverter/runs/openlane_test
 """)
 parser.add_argument('--or-script', '-s', required=True, help='Name of the OpenROAD script causing the failure: i.e. or_antenna_check.tcl, or_pdn.tcl, etc.')
 parser.add_argument('--pdk-root', required=(os.getenv("PDK_ROOT") is None), default=os.getenv("PDK_ROOT"), help='Path to the PDK root (Required if environment variable PDK_ROOT is not set.)')
+parser.add_argument('-c', '--compression', default="gzip", help='Comma,delimited list of compression techniques to use after tar. Use "None" to disable compression altogether. Default is "gzip", which will create a .tar.gzip file. Valid technologies: gzip/gz, xzip/xz, bzip2/bz2')
 parser.add_argument('run_path', help='Path to the run folder.')
-parser.add_argument('--no-tarball', action="store_true", help="Do not create a tarball.")
 args = parser.parse_args()
 
 script_path = abspath(args.or_script)
 run_path = abspath(args.run_path)
 pdk_root = abspath(args.pdk_root)
-tarball = not args.no_tarball
+compression = args.compression
 or_scripts_path = join(openlane_path, "scripts", "openroad")
 
 if not script_path.startswith(or_scripts_path):
@@ -185,9 +182,9 @@ for key in envs_used:
     else:
         final_env_pairs.append((key, value))  
 
-# Phase 5: Create Run File
-run_cmd = join(destination_folder, "run")
-with open(run_cmd, "w") as f:
+# Phase 5: Create Run Files
+run_ol = join(destination_folder, "run_ol")
+with open(run_ol, "w") as f:
     env_list = "\\\n    ".join([f"-e {key}='{value}'" for key, value in final_env_pairs])
     f.write(f"""
 #!/bin/sh
@@ -198,13 +195,38 @@ docker run --rm\\
     {env_list}\\
     {os.getenv("IMAGE_NAME") or "efabless/openlane:current"} openroad \\$::env\\(OR_SCRIPT\\)
     """)
-os.chmod()
+os.chmod(run_ol, 0o755)
+
+run_raw = join(destination_folder, "run")
+with open(run_raw, "w") as f:
+    env_list = "\n".join([f"export {key}='{value}';" for key, value in final_env_pairs])
+    f.write(f"""
+#!/bin/sh
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir;
+{env_list}
+openroad $OR_SCRIPT
+    """)
+os.chmod(run_raw, 0o755)
 
 # Phase 6: Tarball and output
 last_output = destination_folder
-if tarball:
-    last_output = destination_folder + ".tar.gz"
-    os.system(f"tar -cv {destination_folder} | xz | gzip > {last_output}")
+if compression != "None":
+    technology_dict = {
+        "xz": ("xz", "xz"),
+        "xzip": ("xz", "xz"),
+        "gz": ("gzip", "gz"),
+        "gzip": ("gzip", "gz"),
+        "bz2": ("bzip2", "bz2"),
+        "bzip2": ("bzip2", "bz2"),
+    }
+    technologies = compression.split(',')
 
-print("Done.", file=sys.stderr)
+    ext = ".".join([technology_dict[k][1] for k in technologies])
+    last_output = f"{destination_folder}.tar.{ext}"
+
+    pipes = " ".join([f"| {technology_dict[k][0]}" for k in technologies])
+    os.system(f"tar -cvC {destination_folder} . {pipes} > {last_output}")
+
+print("✔ Done.", file=sys.stderr)
 print(last_output)
