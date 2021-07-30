96fc501c850ecb8f3fb27788229abf5e8e671924
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2021-07-30 18:15:20 +0200

    report temporary fix

--- scripts/sta.tcl
+++ scripts/sta.tcl
@@ -40,15 +40,14 @@ report_wns > $::env(opensta_report_file_tag)_wns.rpt
 puts "wns_report_end"
 # report_power
 # report_power > $::env(opensta_report_file_tag)_power.rpt
-
 puts "timing_report"
-report_checks -fields {capacitance slew input_pins nets fanout} -unique -slack_max -0.0 -group_count 100 > $::env(cts_report_file_tag).timing.rpt
+report_checks -fields {capacitance slew input_pins nets fanout} -unique -slack_max -0.0 -group_count 100 > $::env(opensta_report_file_tag).timing.rpt
 puts "timing_report_end"
 puts "min_max_report"
-report_checks -fields {capacitance slew input_pins nets fanout} -path_delay min_max > $::env(cts_report_file_tag).min_max.rpt
+report_checks -fields {capacitance slew input_pins nets fanout} -path_delay min_max > $::env(opensta_report_file_tag).min_max.rpt
 puts "min_max_report_end"
 puts "check_report"
-report_checks -fields {capacitance slew input_pins nets fanout} -group_count 100  -slack_max -0.01 > $::env(cts_report_file_tag).rpt
+report_checks -fields {capacitance slew input_pins nets fanout} -group_count 100  -slack_max -0.01 > $::env(opensta_report_file_tag).rpt
 puts "check_report_end"
 puts "check_slew"
 report_check_types -max_slew -max_capacitance -max_fanout -violators > $::env(opensta_report_file_tag).slew.rpt
