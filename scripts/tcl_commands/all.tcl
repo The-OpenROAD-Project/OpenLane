b6e0dd51bc31be3e94131c59e1393f05c30caa2a
Kareem Farid <kareemfarid@aucegypt.edu>
2021-08-31 20:23:47 +0200

    escape env variables that have $ in them

--- scripts/tcl_commands/all.tcl
+++ scripts/tcl_commands/all.tcl
@@ -22,7 +22,9 @@ proc save_state {args} {
     set_log ::env(PDK_ROOT) $::env(PDK_ROOT) $::env(GLB_CFG_FILE) 1
     foreach index [lsort [array names ::env]] {
         if { $index != "INIT_ENV_VAR_ARRAY" && $index != "PS1" } {
-            set_log ::env($index) [string map {\" \\\"} $::env($index)] $::env(GLB_CFG_FILE) 1
+            set escapped_env_var [string map {\" \\\"} $::env($index)]
+            set escapped_env_var [string map {\$ \\\$} $::env($index)]
+            set_log ::env($index) $escapped_env_var env(GLB_CFG_FILE) 1
         }
     }
 }
