6905a12d2efe18502c37c3207b5ee84cdf720d9c
Donn <me@donn.website>
2021-09-19 11:47:53 +0200

    Revert #607, #601 (#619)
    - #607 - Lack of legalization was causing routing issues with OpenROAD.
    
    - #601 - There is a cryptic "maze ripup wrong" message that makes aes_cipher fail: see The-OpenROAD-Project/OpenROAD#1110
    
    - CU of aes_cipher reduced to 40 (50 was causing issues with TR)
    
    - Fixed or_issue

--- designs/aes_cipher/config.tcl
+++ designs/aes_cipher/config.tcl
@@ -8,6 +8,8 @@ set ::env(CLOCK_PORT) "clk"
 
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
+set ::env(FP_CORE_UTIL) 40
+
 
 set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
