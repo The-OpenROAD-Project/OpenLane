c8907291611d6370b201544e62e20426b56811fa
Mehdi Saligane <48068702+msaligane@users.noreply.github.com>
2021-12-23 10:55:28 -0500

    Update apply_fix.tcl

--- scripts/apply_fix.tcl
+++ scripts/apply_fix.tcl
@@ -1,3 +1,17 @@
+# Copyright 2021 The University of Michigan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 proc move_to_dir {filenames dirname} {
     foreach filename $filenames {
         file rename $filename [file join $dirname [file tail $filename]]
