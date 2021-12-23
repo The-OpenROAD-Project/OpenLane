23e033c527a756c27cd81a04b3a6ca84b40ff04d
Mehdi Saligane <48068702+msaligane@users.noreply.github.com>
2021-12-23 10:54:54 -0500

    Update flow.tcl

--- flow.tcl
+++ flow.tcl
@@ -1,5 +1,6 @@
 #!/usr/bin/tclsh
 # Copyright 2020-2021 Efabless Corporation
+# ECO Flow Copyright 2021 The University of Michigan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
