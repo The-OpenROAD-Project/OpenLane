4aff44eb405654e93ea87feca0b3201b831d471e
Donn <me@donn.website>
2021-12-27 14:34:20 +0200

    Consolidate DEF Manipulation Scripts (#807)
    + Created defutil.py with functionality of all removed scripts (save remove_empty_pins and remove_empty_ports,) reimplemented properly using odb instead of regular expressions
    ~ Folded consoletext.py into the padframe generator
    - Removed li1 hack commands, using the RT_MAX_LAYER/RT_MIN_LAYER variables should be sufficient now that it's passed on to DRT
    - Removed a huge number of shell and python regex-based DEF manipulation scripts

--- scripts/apply_def_template.py
+++ scripts/apply_def_template.py
@@ -13,92 +13,92 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import re
+import click
+import shutil
 import argparse
 import subprocess
-import re
 
-parser = argparse.ArgumentParser(
-    description='Applies a template DEF to a user DEF.')
+@click.command()
+@click.option("-t", "--def-template", "templateDEF", required=True, help="Template DEF")
+@click.argument("userDEF")
+def cli(templateDEF, userDEF):
+    scriptsDir = os.path.dirname(__file__)
 
-parser.add_argument('--templateDEF', '-t',required=True,
-                    help='Template DEF')
+    def remove_power_pins(DEF):
+        templateDEFOpener = open(DEF,"r")
+        if templateDEFOpener.mode == 'r':
+            templateDEFSections =templateDEFOpener.read().split("PINS")
+        templateDEFOpener.close()
+        PINS = templateDEFSections[1].split("- ")
+        OUT_PINS=[" ;"]
+        cnt = 0
+        for pin in PINS[1:]:
+            if pin.find("USE GROUND") + pin.find("USE POWER") == -2:
+                cnt+=1 
+                OUT_PINS.append(pin)
+        OUT_PINS[0] = " "+str(cnt) + OUT_PINS[0] + PINS[0].split(";")[1]
+        OUT_PINS[-1] = OUT_PINS[-1].replace("END ", "")
+        OUT_PINS[-1] = OUT_PINS[-1] + "END "
+        templateDEFSections[1] = "- ".join(OUT_PINS)
+        templateDEFOpener = open(DEF,"w")
+        templateDEFOpener.write("PINS".join(templateDEFSections))
+        templateDEFOpener.close()
+        
+    newTemplateDEF = f"{userDEF}.template.tmp"
+    shutil.copy(templateDEF, newTemplateDEF)
+    templateDEF = newTemplateDEF
 
-parser.add_argument('--userDEF', '-u',required=True,
-                   help='User DEF')
-
-parser.add_argument('--scriptsDir', '-s',required=True,
-                   help='scripts Directory')
-
-args = parser.parse_args()
-templateDEF = args.templateDEF
-userDEF = args.userDEF
-scriptsDir = args.scriptsDir
+    templateDEF = f"{userDEF}.template.tmp"
+    remove_power_pins(templateDEF)
 
 
-def remove_power_pins(DEF):
-    templateDEFOpener = open(DEF,"r")
+    subprocess.check_output([
+        "openroad",
+        "-python",
+        f"{scriptsDir}/defutil.py",
+        "--output", userDEF,
+        "--input-lef", "/dev/null",
+        userDEF, templateDEF
+    ], stderr=subprocess.PIPE)   
+
+    #read template Def
+    templateDEFOpener = open(templateDEF,"r")
     if templateDEFOpener.mode == 'r':
-        templateDEFSections =templateDEFOpener.read().split("PINS")
+        templateDEFContent =templateDEFOpener.read()
     templateDEFOpener.close()
-    PINS = templateDEFSections[1].split("- ")
-    OUT_PINS=[" ;"]
-    cnt = 0
-    for pin in PINS[1:]:
-        if pin.find("USE GROUND") + pin.find("USE POWER") == -2:
-            cnt+=1 
-            OUT_PINS.append(pin)
-    OUT_PINS[0] = " "+str(cnt) + OUT_PINS[0] + PINS[0].split(";")[1]
-    OUT_PINS[-1] = OUT_PINS[-1].replace("END ", "")
-    OUT_PINS[-1] = OUT_PINS[-1] + "END "
-    templateDEFSections[1] = "- ".join(OUT_PINS)
-    templateDEFOpener = open(DEF,"w")
-    templateDEFOpener.write("PINS".join(templateDEFSections))
-    templateDEFOpener.close()
-    
-
-command = 'cp {templateDEF} {newtemplateDEF}'.format(templateDEF=templateDEF, newtemplateDEF=userDEF+".template.tmp")
-subprocess.check_output(command.split(), stderr=subprocess.PIPE) 
-
-templateDEF = userDEF+".template.tmp"
-remove_power_pins(templateDEF)
 
 
-command = 'sh {scriptsDir}/mv_pins.sh {templateDEF} {userDEF}'.format(scriptsDir=scriptsDir,templateDEF=templateDEF, userDEF=userDEF)
-subprocess.check_output(command.split(), stderr=subprocess.PIPE)   
-
-#read template Def
-templateDEFOpener = open(templateDEF,"r")
-if templateDEFOpener.mode == 'r':
-    templateDEFContent =templateDEFOpener.read()
-templateDEFOpener.close()
-
-
-#read user Def
-userDEFOpener = open(userDEF,"r")
-if userDEFOpener.mode == 'r':
-    userDEFContent =userDEFOpener.read()
-userDEFOpener.close()
-
-
-def copyStringWithWord(word, f_rom, t_o):
-    pattern = re.compile(r'\b%s\b\s*\([^)]*\)\s*\([^)]*\)' % word)
-    instances = re.findall(pattern, f_rom)
-    if len(instances) == 1:
-        str_from = instances[0]
-        tmp = re.sub(pattern, str_from, t_o)
-        return tmp
-    return None
-
-
-
-# Copy DIEAREA
-word='DIEAREA'
-userDEFContent = copyStringWithWord(word, templateDEFContent, userDEFContent)
-
-
-if userDEFContent is not None:
-    userDEFOpener = open(userDEF,"w")
-    userDEFOpener.write(userDEFContent)
+    #read user Def
+    userDEFOpener = open(userDEF,"r")
+    if userDEFOpener.mode == 'r':
+        userDEFContent =userDEFOpener.read()
     userDEFOpener.close()
-else:
-    raise Exception("DIEAREA not found in DEF")
+
+
+    def copyStringWithWord(word, f_rom, t_o):
+        pattern = re.compile(r'\b%s\b\s*\([^)]*\)\s*\([^)]*\)' % word)
+        instances = re.findall(pattern, f_rom)
+        if len(instances) == 1:
+            str_from = instances[0]
+            tmp = re.sub(pattern, str_from, t_o)
+            return tmp
+        return None
+
+
+
+    # Copy DIEAREA
+    word='DIEAREA'
+    userDEFContent = copyStringWithWord(word, templateDEFContent, userDEFContent)
+
+
+    if userDEFContent is not None:
+        userDEFOpener = open(userDEF,"w")
+        userDEFOpener.write(userDEFContent)
+        userDEFOpener.close()
+    else:
+        raise Exception("DIEAREA not found in DEF")
+
+if __name__ == '__main__':
+    cli()
