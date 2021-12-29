5ac86dcc48fd72d786c4bf28ceb701e2f993e641
Mohamed Gaber <me@donn.website>
2021-12-29 08:40:04 +0200

    Remove clk_nets, already obtained from SDC\nResolves #769

--- scripts/openroad/cts.tcl
+++ scripts/openroad/cts.tcl
@@ -58,7 +58,6 @@ set arg_list [list]
 
 lappend arg_list -buf_list $::env(CTS_CLK_BUFFER_LIST)
 lappend arg_list -root_buf $::env(CTS_ROOT_BUFFER)
-lappend arg_list -clk_nets $::env(CLOCK_NET)
 lappend arg_list -sink_clustering_size $::env(CTS_SINK_CLUSTERING_SIZE)
 lappend arg_list -sink_clustering_max_diameter $::env(CTS_SINK_CLUSTERING_MAX_DIAMETER)
 lappend arg_list -sink_clustering_enable 
