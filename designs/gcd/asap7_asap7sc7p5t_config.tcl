648000e61493e7856ad186686898fd3862e66b02
Matt Liberty <mliberty@eng.ucsd.edu>
2022-02-07 11:43:11 -0800

    add sky130hs/hd setup for gcd
    Signed-off-by: Matt Liberty <mliberty@eng.ucsd.edu>

--- designs/gcd/asap7_asap7sc7p5t_config.tcl
+++ designs/gcd/asap7_asap7sc7p5t_config.tcl
@@ -2,7 +2,8 @@ set ::env(RUN_MAGIC) {0}
 set ::env(RUN_MAGIC_DRC) {0}
 set ::env(RUN_LVS) {0}
 set ::env(PRIMARY_SIGNOFF_TOOL) {klayout}
-set ::env(RUN_KLAYOUT_DRC) {1}
+
+set ::env(CLOCK_PERIOD) "420"
 
 set ::env(FP_SIZING) "absolute"
 set ::env(DIE_AREA) "0 0 16.2 16.2"
