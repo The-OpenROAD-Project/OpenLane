645b95f6bec2b9bbf6dea8c6f44449bd9df6e778
Matt Liberty <mliberty@eng.ucsd.edu>
2022-03-01 10:10:21 -0800

    Further align gcd test case to match ORFS (#971)

--- designs/gcd/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/gcd/sky130A_sky130_fd_sc_hd_config.tcl
@@ -4,5 +4,11 @@ set ::env(FP_SIZING) "absolute"
 set ::env(DIE_AREA) "0 0 279.96 280.128"
 set ::env(PLACE_DENSITY) 0.35
 
-set ::env(LEFT_MARGIN_MULT)  20
-set ::env(RIGHT_MARGIN_MULT) 20
+set ::env(LEFT_MARGIN_MULT)  22
+set ::env(RIGHT_MARGIN_MULT) 22
+set ::env(TOP_MARGIN_MULT)    3
+
+set ::env(FP_PDN_VPITCH)  27.14
+set ::env(FP_PDN_HPITCH)  27.20
+set ::env(FP_PDN_VOFFSET) 13.57
+set ::env(FP_PDN_HOFFSET) 13.60
