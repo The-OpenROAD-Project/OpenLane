7a3a287d96c8d76b2627eab7b12103509de7fd99
Donn <me@donn.website>
2022-04-02 18:05:01 +0000

    Update OpenROAD, Restore post-GR timing optimizations

--- designs/salsa20/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/salsa20/sky130A_sky130_fd_sc_hd_config.tcl
@@ -2,6 +2,5 @@
 set ::env(CLOCK_PERIOD) "38"
 set ::env(FP_CORE_UTIL) 20
 set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.15
-set ::env(CTS_CLK_BUFFER_LIST) "sky130_fd_sc_hd__clkbuf_4 sky130_fd_sc_hd__clkbuf_8"
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.25
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
--- designs/y_huff/sky130A_sky130_fd_sc_hd_config.tcl
+++ designs/y_huff/sky130A_sky130_fd_sc_hd_config.tcl
@@ -2,5 +2,4 @@
 set ::env(CLOCK_PERIOD) "14"
 set ::env(FP_CORE_UTIL) 40
 set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(CTS_CLK_BUFFER_LIST) "sky130_fd_sc_hd__clkbuf_4 sky130_fd_sc_hd__clkbuf_8"
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
