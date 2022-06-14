The-OpenROAD-Project/OpenLane

commit b29b71536b644dd76550dfdfcc09319b4ed86a79

Python Cleanup (#864)
+ Consolidated all existing DRC conversion functions into `drc_rosetta.py`
+ Consolidated fake_diode_replace and place_diode into `diode.py`
+ Consolidated and/or rewrote most def manipulation functions into `defutil.py` and other files under `odbpy/`.
+ Consolidated and/or rewrote most lef manipulation functions into `lefutil.py`
~ Moved all files using odb to a new folder, `odbpy/`
~ Move replicate/update config scripts to `scripts/config` folder
~ Move all in-line magic spells into proper files
~ Tcl formatting (4 space-indents on all modified files)
- Removed many unused files, all consolidated files
---
API Breaks

* Care was taken to ensure all OpenLane tcl commands have the same API presuming the API in question was not stupid.
    * Speaking of, `manual_macro_placement_test f` has been updated to `manual_macro_placement_test -f` to be consistent with literally every other command.
* All individual files that may be used by third party projects likely have a different CLI and are thus entirely broken.

scripts/tcl_commands/all.tcl
@@ -15,7 +15,6 @@
 package require json
 package require openlane_utils
 
-
 proc save_state {args} {
     set ::env(INIT_ENV_VAR_ARRAY) [split [array names ::env] " "]
     puts_info "Saving runtime environment..."
@@ -101,12 +100,17 @@ proc prep_lefs {args} {
     puts_info "Preparing LEF files for the $arg_values(-corner) corner..."
 
     if { $arg_values(-corner) == "nom" } {
-        puts_verbose "Extracting the number of available metal layers from $arg_values(-tech_lef)"
+        puts_verbose "Extracting the number of available metal layers from $arg_values(-tech_lef)..."
 
-        set ::env(TECH_METAL_LAYERS)  [exec python3 $::env(SCRIPTS_DIR)/extract_metal_layers.py $arg_values(-tech_lef)]
+        try_catch openroad -python\
+            $::env(SCRIPTS_DIR)/odbpy/lefutil.py get_metal_layers\
+            -o $::env(TMP_DIR)/layers.list\
+            $arg_values(-tech_lef)
+
+        set ::env(TECH_METAL_LAYERS)  [cat $::env(TMP_DIR)/layers.list]
         set ::env(MAX_METAL_LAYER) [llength $::env(TECH_METAL_LAYERS)]
 
-        puts_verbose "The available metal layers ($::env(MAX_METAL_LAYER)) are $::env(TECH_METAL_LAYERS)"
+        puts_verbose "The available metal layers ($::env(MAX_METAL_LAYER)) are $::env(TECH_METAL_LAYERS)."
         puts_verbose "Merging LEF Files..."
     }
 
@@ -240,7 +244,7 @@ proc trim_lib {args} {
     try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/libtrim.py\
         --cell-file $arg_values(-output).exclude.list\
         --output $arg_values(-output)\
-        $arg_values(-input)
+        {*}$arg_values(-input)
 }
 
 proc source_config {config_file} {
@@ -473,19 +477,6 @@ proc prep {args} {
 
     set skip_basic_prep 0
 
-    if { [file exists $::env(RUN_DIR)] } {
-        if { [info exists flags_map(-overwrite)] } {
-            puts_warn "Removing existing run at $::env(RUN_DIR)..."
-            after 1000
-            file delete -force $::env(RUN_DIR)
-        } elseif { ![info exists flags_map(-last_run)] } {
-            puts_warn "A run for $::env(DESIGN_NAME) with tag '$tag' already exists. Pass the -overwrite option to overwrite it."
-            after 1000
-            set skip_basic_prep 1
-        }
-    }
-
-
     # file mkdir *ensures* they exists (no problem if they already do)
     file mkdir $::env(RESULTS_DIR) $::env(TMP_DIR) $::env(LOGS_DIR) $::env(REPORTS_DIR)
 
@@ -494,15 +485,21 @@ proc prep {args} {
     if { [file exists $::env(GLB_CFG_FILE)] } {
         if { [info exists flags_map(-overwrite)] } {
             puts_info "Removing $::env(GLB_CFG_FILE)"
+            after 1000
             file delete $::env(GLB_CFG_FILE)
         } else {
-            puts_info "Sourcing $::env(GLB_CFG_FILE)\nAny changes to the DESIGN config file will NOT be applied"
+            if { ![info exists flags_map(-last_run)] } {
+                puts_warn "A run for $::env(DESIGN_NAME) with tag '$tag' already exists. Pass the -overwrite option to overwrite it."
+                after 1000
+            }
+            puts_info "Sourcing $::env(GLB_CFG_FILE). Any changes to the DESIGN config file will NOT be applied."
             source $::env(GLB_CFG_FILE)
             if { [info exists ::env(CURRENT_DEF)] && $::env(CURRENT_DEF) != 0 } {
                 puts_info "Current DEF: $::env(CURRENT_DEF)."
                 puts_info "Use 'set_def file_name.def' if you'd like to change it."
             }
             after 1000
+            set skip_basic_prep 1
         }
     }
 
@@ -671,8 +668,8 @@ proc prep {args} {
     }
 
     if { [info exists ::env(EXTRA_GDS_FILES)] } {
-        puts_info "Looking for files defined in ::env(EXTRA_GDS_FILES) $::env(EXTRA_GDS_FILES) ..."
-        assert_files_exist $::env(EXTRA_GDS_FILES)
+        puts_verbose "Verifying existence of files defined in ::env(EXTRA_GDS_FILES)..."
+        assert_files_exist "$::env(EXTRA_GDS_FILES)"
     }
 
 
@@ -683,7 +680,7 @@ proc prep {args} {
             flow_fail
             return -code error
         }
-        
+
         puts_warn "OpenLane may not function properly: $::env(VCHECK_OUTPUT)"
     }
 
@@ -842,24 +839,29 @@ proc heal_antenna_violators {args} {
         increment_index
         TIMER::timer_start
         puts_info "Healing Antenna Violators..."
+        set violators_file [index_file $::env(routing_reports)/antenna_violators.rpt]
         if { $::env(USE_ARC_ANTENNA_CHECK) == 1 } {
             #ARC specific
             if { [info exists ::env(ANTENNA_CHECKER_LOG)] } {
-                try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/extract_antenna_violators.py -i $::env(ANTENNA_CHECKER_LOG) -o [index_file $::env(routing_reports)/violators.txt]
+                try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/extract_antenna_violators.py -i $::env(ANTENNA_CHECKER_LOG) -o $violators_file
             } else {
                 puts_err "Ran heal_antenna_violators without running the antenna check first."
                 flow_fail
             }
         } else {
             #Magic Specific
-            set report_file [open [index_file $::env(routing_reports)/antenna_violators.rpt] r]
-            set violators [split [string trim [read $report_file]]]
-            close $report_file
-            # may need to speed this up for extremely huge files using hash tables
+            set violators [split [string trim [cat $violators_file]]]
             exec echo $violators >> [index_file $::env(routing_reports)/violators.txt]
         }
+
         #replace violating cells with real diodes
-        try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/fake_diode_replace.py -v [index_file $::env(routing_reports)/violators.txt] -d $::env(routing_results)/$::env(DESIGN_NAME).def -f $::env(FAKEDIODE_CELL) -t $::env(DIODE_CELL)
+        try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/odbpy/diodes.py\
+            replace_fake\
+            -o $::env(routing_results)/$::env(DESIGN_NAME).def\
+            -v $violators_file\
+            -f $::env(FAKEDIODE_CELL) -t $::env(DIODE_CELL)\
+            $::env(routing_results)/$::env(DESIGN_NAME).def
+
         TIMER::timer_stop
         exec echo "[TIMER::get_runtime]" | python3 $::env(SCRIPTS_DIR)/write_runtime.py "heal antenna violators - custom"
     }
@@ -873,17 +875,25 @@ proc widen_site_width {args} {
         set ::env(MERGED_LEF_UNPADDED_WIDENED) $::env(MERGED_LEF_UNPADDED)
         set ::env(MERGED_LEF_WIDENED) $::env(MERGED_LEF)
     } else {
-        puts_info "Widenning Site Width..."
+        puts_info "Widening Site Width..."
         set ::env(MERGED_LEF_UNPADDED_WIDENED) $::env(TMP_DIR)/merged_unpadded_wider.lef
         set ::env(MERGED_LEF_WIDENED) $::env(TMP_DIR)/merged_wider.lef
-        if { $::env(WIDEN_SITE_IS_FACTOR) == 1 } {
-            try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/widen_site_lef.py -l $::env(MERGED_LEF_UNPADDED) -w $::env(WIDEN_SITE) -f -o $::env(MERGED_LEF_UNPADDED_WIDENED)
-            try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/widen_site_lef.py -l $::env(MERGED_LEF) -w $::env(WIDEN_SITE) -f -o $::env(MERGED_LEF_WIDENED)
 
-        } else {
-            try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/widen_site_lef.py -l $::env(MERGED_LEF_UNPADDED) -w $::env(WIDEN_SITE) -o $::env(MERGED_LEF_UNPADDED_WIDENED)
-            try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/widen_site_lef.py -l $::env(MERGED_LEF) -w $::env(WIDEN_SITE) -o $::env(MERGED_LEF_WIDENED)
+        set widen_args [list]
+        lappend widen_args --widen-value $::env(WIDEN_SITE)
+        if { $::env(WIDEN_SITE_IS_FACTOR) == 1 } {
+            lappend widen_args --factor
         }
+
+        try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/odbpy/lefutil.py widen_site\
+            {*}$widen_args\
+            --output $::env(MERGED_LEF_UNPADDED_WIDENED)\
+            $::env(MERGED_LEF_UNPADDED)
+
+        try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/odbpy/lefutil.py widen_site\
+            {*}$widen_args\
+            --output $::env(MERGED_LEF_WIDENED)\
+            $::env(MERGED_LEF)
     }
 }
 
@@ -928,13 +938,13 @@ proc label_macro_pins {args} {
 
     set_if_unset arg_values(-pad_pin_name) ""
 
-    try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/label_macro_pins.py\
+    try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/odbpy/label_macro_pins.py\
         --lef $arg_values(-lef)\
-        --input-def $::env(CURRENT_DEF)\
         --netlist-def $arg_values(-netlist_def)\
         --pad-pin-name $arg_values(-pad_pin_name)\
-        -o $output_def\
-        {*}$extra_args |& tee [index_file $::env(signoff_logs)/label_macro_pins.log] $::env(TERMINAL_OUTPUT)
+        --output $output_def\
+        {*}$extra_args $::env(CURRENT_DEF)\
+        |& tee [index_file $::env(signoff_logs)/label_macro_pins.log] $::env(TERMINAL_OUTPUT)
     TIMER::timer_stop
     exec echo "[TIMER::get_runtime]" | python3 $::env(SCRIPTS_DIR)/write_runtime.py "label macro pins - label_macro_pins.py"
 }

run_designs.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import os
 import sys
 import click

scripts/add_def_obstructions.py
@@ -1,100 +1,0 @@
-#!/usr/bin/env python3
-# Copyright 2020 Efabless Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import argparse
-import re
-import odb
-
-parser = argparse.ArgumentParser(description="Creates obstructions in def files.")
-
-parser.add_argument(
-    "--lef",
-    "-l",
-    nargs="+",
-    type=str,
-    default=None,
-    required=True,
-    help="LEF file needed to have a proper view of the DEF files.",
-)
-
-parser.add_argument(
-    "--input-def",
-    "-id",
-    required=True,
-    help="DEF view of the design that needs to be obstructed.",
-)
-
-parser.add_argument(
-    "--obstructions",
-    "-obs",
-    required=True,
-    help="Format: layer llx lly urx ury, ... (in microns)",
-)
-
-parser.add_argument("--output", "-o", required=True, help="Output DEF file.")
-
-args = parser.parse_args()
-
-input_lef_file_names = args.lef
-input_def_file_name = args.input_def
-obs_args = args.obstructions
-output_def_file_name = args.output
-
-RE_NUMBER = r"[\-]?[0-9]+(\.[0-9]+)?"
-RE_OBS = (
-    r"(?P<layer>\S+)\s+"
-    + r"(?P<bbox>"
-    + RE_NUMBER
-    + r"\s+"
-    + RE_NUMBER
-    + r"\s+"
-    + RE_NUMBER
-    + r"\s+"
-    + RE_NUMBER
-    + r")"
-)
-
-obses = obs_args.split(",")
-obs_list = []
-for obs in obses:
-    obs = obs.strip()
-    m = re.match(RE_OBS, obs)
-    assert (
-        m
-    ), "Incorrectly formatted input (%s).\n Format: layer llx lly urx ury, ..." % (obs)
-    layer = m.group("layer")
-    bbox = [float(x) for x in m.group("bbox").split()]
-    obs_list.append((layer, bbox))
-
-design_db = odb.dbDatabase.create()
-
-for lef in input_lef_file_names:
-    odb.read_lef(design_db, lef)
-
-odb.read_def(design_db, input_def_file_name)
-
-design_chip = design_db.getChip()
-design_block = design_chip.getBlock()
-design_insts = design_block.getInsts()
-design_tech = design_db.getTech()
-
-for obs in obs_list:
-    layer = obs[0]
-    bbox = obs[1]
-    dbu = design_tech.getDbUnitsPerMicron()
-    bbox = [int(x * dbu) for x in bbox]
-    print("Creating an obstruction on", layer, "at", *bbox, "(DBU)")
-    odb.dbObstruction_create(design_block, design_tech.findLayer(layer), *bbox)
-
-odb.write_def(design_block, output_def_file_name)

scripts/contextualize.py
@@ -1,167 +1,0 @@
-#!/usr/bin/env python3
-# Copyright 2020 Efabless Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import argparse
-import odb
-
-parser = argparse.ArgumentParser(
-    description="Produces a DEF file where a design is shown in the context of its instantiation in a top-level design"
-)
-
-parser.add_argument("--macro-def", "-md", required=True, help="DEF view of the design")
-
-parser.add_argument(
-    "--macro-lef",
-    "-ml",
-    required=True,
-    help="LEF file needed to have a proper view of the macro DEF",
-)
-
-parser.add_argument(
-    "--top-def",
-    "-td",
-    required=True,
-    help="DEF view of the top-level design where the macro is instantiated",
-)
-parser.add_argument(
-    "--top-lef",
-    "-tl",
-    required=True,
-    help="LEF file needed to have a proper view of the top-level DEF",
-)
-
-parser.add_argument(
-    "--output",
-    "-o",
-    required=True,
-    default="output.def",
-    help="Output Contextualized DEF",
-)
-
-parser.add_argument(
-    "--keep-inner-connections",
-    "-keep",
-    action="store_true",
-    default=False,
-    help="If set, the internal cells will remain conneted in the otput DEF",
-)
-
-
-args = parser.parse_args()
-
-macro_def_file_name = args.macro_def
-macro_lef_file_name = args.macro_lef
-top_def_file_name = args.top_def
-top_lef_file_name = args.top_lef
-keep_flag = args.keep_inner_connections
-
-output_def_file_name = args.output
-
-# in the tcl side
-# top_lef_new_file_name = os.path.join(os.path.dirname(macro_lef_file_name), os.path.basename(macro_lef_file_name) + ".top.lef")
-# top_def_new_file_name = os.path.join(os.path.dirname(macro_def_file_name), os.path.basename(macro_def_file_name) + ".top.def")
-
-# copy(os.path.normpath(top_lef_file_name), top_lef_new_file_name)
-# copy(os.path.normpath(top_def_file_name), top_def_new_file_name)
-
-# top_lef_file_name = top_lef_new_file_name
-# top_def_file_name = top_def_new_file_name
-
-db_macro = odb.dbDatabase.create()
-db_top = odb.dbDatabase.create()
-odb.read_lef(
-    db_macro, top_lef_file_name
-)  # must read first to have consistent views with the top-level
-odb.read_lef(
-    db_macro, macro_lef_file_name
-)  # rest of the macros that don't appear in the top-level are covered here
-odb.read_def(db_macro, macro_def_file_name)
-
-odb.read_lef(db_top, top_lef_file_name)
-odb.read_def(db_top, top_def_file_name)
-
-chip_macro = db_macro.getChip()
-block_macro = chip_macro.getBlock()
-macro_design_name = block_macro.getName()
-
-chip_top = db_top.getChip()
-block_top = chip_top.getBlock()
-top_design_name = block_top.getName()
-
-print("Block design name:", macro_design_name)
-print("Top-level design name:", top_design_name)
-
-nets_top = block_top.getNets()
-to_connect = {}
-MACRO_TOP_PLACEMENT_X = 0
-MACRO_TOP_PLACEMENT_Y = 0
-MACRO_TOP_PLACEMENT_ORIENT = 0
-
-assert macro_design_name in [
-    inst.getMaster().getName() for inst in block_top.getInsts()
-], "%s not found in %s" % (macro_design_name, top_design_name)
-
-for net in nets_top:
-    iterms = net.getITerms()  # asssumption: no pins (bterms) on top level
-    block_net_name = None
-    for iterm in iterms:
-        macro_name = iterm.getMTerm().getMaster().getName()
-        if macro_name == macro_design_name:
-            block_net_name = iterm.getMTerm().getName()
-            macro_top_inst = iterm.getInst()
-            MACRO_TOP_PLACEMENT_X, MACRO_TOP_PLACEMENT_Y = macro_top_inst.getLocation()
-            MACRO_TOP_PLACEMENT_ORIENT = macro_top_inst.getOrient()
-            print("Block net name: ", block_net_name)
-            break
-    if block_net_name is not None:
-        to_connect[block_net_name] = []
-        for iterm in iterms:
-            macro_name = iterm.getMTerm().getMaster().getName()
-            if macro_name != macro_design_name:
-                to_connect[block_net_name].append(iterm)
-        block_net_name = None
-
-        # print(macro_name, inst_name, end= ' ')
-        # print(iterm.getMTerm().getName())
-
-# print(to_connect)
-
-nets_macro = block_macro.getNets()
-created_macros = {}
-for net in nets_macro:
-    iterms = net.getITerms()  # asssumption: no pins (bterms) on top level
-    if not keep_flag:
-        for iterm in iterms:
-            iterm.disconnect()
-    if net.getName() in to_connect:
-        for node_iterm in to_connect[net.getName()]:
-            node_master = node_iterm.getMTerm().getMaster()
-            node_inst = node_iterm.getInst()
-            node_inst_name = node_iterm.getInst().getName()
-            if node_inst_name not in created_macros:
-                created_macros[node_inst_name] = 1
-                print("Creating: ", node_master.getName(), node_inst_name)
-                new_inst = odb.dbInst_create(block_macro, node_master, node_inst_name)
-                new_inst.setOrient(node_inst.getOrient())
-                new_inst.setLocation(
-                    node_inst.getLocation()[0] - MACRO_TOP_PLACEMENT_X,
-                    node_inst.getLocation()[1] - MACRO_TOP_PLACEMENT_Y,
-                )
-                new_inst.setPlacementStatus("FIRM")
-            else:
-                new_inst = block_macro.findInst(node_inst_name)
-            new_inst.findITerm(node_iterm.getMTerm().getName()).connect(net)
-
-odb.write_def(block_macro, output_def_file_name)

commit 074a92b7895be96a66db4318015ecb9f2c0f93c2

Fix Magic Antenna Check (#1142)
~ Fixed issue where `run_magic_antenna_check` would fail to run after regression in #864 
~ Fixed issue where `run_magic_antenna_check` processed output aliases the raw report
~ Fix `most_recent_run.py`

scripts/most_recent_run.py
@@ -32,6 +32,7 @@ def main(runs_directory):
             continue
         time = os.stat(run_folder_path).st_mtime
         if time > max_time:
+            max_time = time
             latest_run_name = run
 
     if latest_run_name is not None:

scripts/tcl_commands/all.tcl
@@ -843,8 +843,8 @@ proc heal_antenna_violators {args} {
         set violators_file [index_file $::env(routing_reports)/antenna_violators.rpt]
         if { $::env(USE_ARC_ANTENNA_CHECK) == 1 } {
             #ARC specific
-            if { [info exists ::env(ANTENNA_CHECKER_LOG)] } {
-                try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/extract_antenna_violators.py -i $::env(ANTENNA_CHECKER_LOG) -o $violators_file
+            if { [info exists ::env(ANTENNA_CHECKER_REPORT)] } {
+                try_catch $::env(OPENROAD_BIN) -python $::env(SCRIPTS_DIR)/extract_antenna_violators.py -i $::env(ANTENNA_CHECKER_REPORT) -o $violators_file
             } else {
                 puts_err "Ran heal_antenna_violators without running the antenna check first."
                 flow_fail
@@ -1000,7 +1000,9 @@ proc run_or_antenna_check {args} {
     puts_info "Running OpenROAD Antenna Rule Checker..."
     set antenna_log [index_file $::env(signoff_logs)/antenna.log]
     run_openroad_script $::env(SCRIPTS_DIR)/openroad/antenna_check.tcl -indexed_log $antenna_log
-    set ::env(ANTENNA_CHECKER_LOG) $antenna_log
+    set antenna_rpt [index_file $::env(signoff_reports)/antenna.rpt]
+    exec cp $antenna_log $antenna_rpt
+    set ::env(ANTENNA_CHECKER_REPORT) $antenna_rpt
     TIMER::timer_stop
     exec echo "[TIMER::get_runtime]" | python3 $::env(SCRIPTS_DIR)/write_runtime.py "antenna check - openroad"
 }

scripts/tcl_commands/magic.tcl
@@ -215,12 +215,8 @@ proc run_magic_antenna_check {args} {
     increment_index
     TIMER::timer_start
     puts_info "Running Magic Antenna Checks..."
-    set feedback_file [index_file $::env(signoff_logs)/ext2spice.antenna.feedback.txt]
-    set magic_export $::env(signoff_tmpfiles)/magic_antenna.tcl
+    set feedback_file [index_file $::env(signoff_reports)/antenna.feedback.txt]
 
-    set magic_export_file [open $magic_export w]
-    puts $magic_export_file $commands
-    close $magic_export_file
     # the following MAGTYPE has to be mag; antennacheck needs to know
     # about the underlying devices, layers, etc.
     set ::env(MAGTYPE) mag
@@ -237,10 +233,12 @@ proc run_magic_antenna_check {args} {
         |& tee $::env(TERMINAL_OUTPUT) $antenna_log
     unset ::env(_tmp_feedback_file)
 
-    # process the log
-    try_catch awk "/Cell:/ {print \$2}" $antenna_log > $antenna_log
+    set antenna_rpt [index_file $::env(signoff_reports)/antenna.rpt]
 
-    set ::env(ANTENNA_CHECKER_LOG) $antenna_log
+    # process the log
+    try_catch awk "/Cell:/ {print \$2}" $antenna_log > $antenna_rpt
+
+    set ::env(ANTENNA_CHECKER_REPORT) $antenna_rpt
 
     TIMER::timer_stop
     exec echo "[TIMER::get_runtime]" | python3 $::env(SCRIPTS_DIR)/write_runtime.py "antenna check - magic"