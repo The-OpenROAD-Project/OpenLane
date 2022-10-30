2af9b9f5787ed784050ae9ad4b7c30020bd507d7
Olof Kindgren <olof.kindgren@gmail.com>
2022-10-30 23:58:27 +0100

    Add FuseSoC support for spm design
    This adds a core description file for the spm core that exposes targets
    for linting and for building a GDSII using OpenLANE.
    
    Quick FuseSoC instructions:
    
     #install FuseSoC
    pip3 install fusesoc
     #Create and enter a new workspace
    mkdir workspace && cd workspace
     #Register spm as a library in the workspace
    fusesoc library add spm /path/to/spm
     #...if repo is available locally or...
    fusesoc library add spm https://github.com/The-OpenROAD-Project/OpenLane
     #...to get the upstream repo
    
     #To run lint
    fusesoc run --target=lint efabless::spm
     #To build with OpenLANE running in a docker container
    EDALIZE_LAUNCHER=el_docker fusesoc run --target=sky130 efabless::spm
     #List all targets
    fusesoc core show efabless::spm

---
+++ designs/spm/sky130.tcl
@@ -0,0 +1,9 @@
+set ::env(DESIGN_NAME) "spm"
+set ::env(CLOCK_PERIOD) 10
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_NET) "clk"
+set ::env(FP_PDN_VOFFSET) 7
+set ::env(FP_PDN_HOFFSET) 7
+set ::env(FP_PIN_ORDER_CFG) "pin_order.cfg"
+set ::env(FP_PDN_SKIPTRIM) true
+set ::env(FP_CORE_UTIL) 45
