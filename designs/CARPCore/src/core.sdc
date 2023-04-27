e290d370d6ab24f16031abfb2fdb9a9e496233dc
nklimov-dev <nikklim16@gmail.com>
2023-04-27 01:51:20 +0000

    exploration on wire length

--- designs/CARPCore/src/core.sdc
+++
@@ -1,2 +0,0 @@
-set_units -time ns
-create_clock [get_ports clk_i]  -name core_clock  -period 10
--- designs/core/run_exploration.sh
+++ designs/core/run_exploration.sh
@@ -1,2 +1,2 @@
  
-python3 run_designs.py --config_file /openlane/designs/core/config.json --matrix /openlane/designs/core/matrix.json -j 1 --delete --print_rem 1800 core
+python3 run_designs.py --config_file config.json --matrix ./designs/core/matrix.json -j 1 --delete --print_rem 1800 core
