c9ef4a000628fedda9895e1b1e273d3d8ebd4def
nklimov-dev <nikklim16@gmail.com>
2023-05-09 01:11:30 +0000

    matrix results 1

--- designs/sv_core/run_exploration.sh
+++ designs/sv_core/run_exploration.sh
@@ -1,2 +1,2 @@
  
-python3 run_designs.py --config_file config.json --matrix ./designs/sv_core/matrix.json -j 4 --delete --print_rem 1800 sv_core
+python3 run_designs.py --config_file config.json --matrix ./designs/sv_core/matrix.json -j 2 --delete --print_rem 1800 sv_core
