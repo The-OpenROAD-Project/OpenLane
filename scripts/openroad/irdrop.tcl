ff7ccf6545a1fd283bc95b7f75aea846536efe8e
kareefardi <kareem.farid@efabless.com>
2023-06-20 13:27:49 +0300

    fix condition when VSRC_LOC_FILES is defined

--- scripts/openroad/irdrop.tcl
+++ scripts/openroad/irdrop.tcl
@@ -21,7 +21,8 @@ if { [info exists ::env(VSRC_LOC_FILES)] } {
     foreach {net vsrc_file} "$::env(VSRC_LOC_FILES)" {
         set arg_list [list]
         lappend -net $net
-        lappend -out $::env(_tmp_save_rpt_prefix)-$net.rpt
+        lappend -outfile $::env(_tmp_save_rpt_prefix)-$net.rpt
+        lappend -vsrc $vsrc_file
         analyze_power_grid {*}$arg_list
     }
 } else {
