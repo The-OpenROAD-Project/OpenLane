ed5647b8c8a83946bb040df4a84c7e6d5e16ce05
Kareem Farid <kareefardi@users.noreply.github.com>
2023-07-12 14:38:52 +0300

    Undocumented Variable CI Workflow
    Script uses regular expressions to create a set of documents variables and a set of used variables and compare them against each other. Some variables are internal, unexposed and others. These variables are whitelisted. See https://github.com/The-OpenROAD-Project/OpenLane/issues/1889
    
    \+ Add documentation for `QUIT_ON_XOR_ERROR`

--- configuration/general.tcl
+++ configuration/general.tcl
@@ -26,12 +26,11 @@ set ::env(RUN_FILL_INSERTION) 1
 set ::env(RUN_TAP_DECAP_INSERTION) 1
 set ::env(RUN_LINTER) 1
 
-## Intentionally Undocumented
-set ::env(RSZ_USE_OLD_REMOVER) 0
-
 ## STA
 set ::env(STA_REPORT_POWER) {1}
 set ::env(STA_WRITE_LIB) {1}
+
+### Private: Not granular enough, not going to be compatible with OL2
 set ::env(STA_MULTICORNER_READ_LIBS) 0
 
 ## Routing
