0075fc3b89741e2f5be7984220308a8a658804c2
kareefardi <kareem.farid@efabless.com>
2023-11-10 12:35:17 +0200

    also check for lvs report file

--- tests/2038-lvs_good/issue_regression.py
+++ tests/2038-lvs_good/issue_regression.py
@@ -9,14 +9,16 @@ run_folder = args[0]
 
 log_path = glob.glob(
     os.path.join(run_folder, "reports", "signoff", "*-inverter.lvs.rpt")
-)[0]
+)
+assert len(log_path) != 0, "LVS report file not found"
+assert len(log_path) == 1, "Multiple LVS report files found"
 assert (
     subprocess.call(
         [
             "grep",
             "-i",
             "Total errors = 0",
-            log_path,
+            log_path[0],
         ]
     )
     == 0
