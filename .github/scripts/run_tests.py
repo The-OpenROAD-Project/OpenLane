56d0027ce57a64e95fa2f3305e82d9e36b82fd25
Mohamed Gaber <donn@efabless.com>
2024-04-08 02:36:41 +0200

    brainrot

--- .github/scripts/run_tests.py
+++ .github/scripts/run_tests.py
@@ -53,7 +53,7 @@ docker_command = [
     f"PDK_ROOT={gh.pdk_root}",
     "-e",
     f"PDK={gh.pdk}",
-    "efabless/openlane2:current-amd64",
+    "efabless/openlane:current-amd64",
     "bash",
     "-c",
     shlex.join(
